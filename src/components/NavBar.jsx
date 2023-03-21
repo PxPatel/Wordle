3ebc231fa51bbab31ae68538abd4dc1e89503dc4
Priyansh <87282275+PriIrp@users.noreply.github.com>
2023-03-21 20:49:32 +0000

    Added New Hooks

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import useDarkMode from '../hooks/useDarkMode';
 
 const NavBar = () => {
-  return (
+  const [darkTheme, setDarkTheme] = useDarkMode();
+  const handleMode = () => setDarkTheme(!darkTheme);
+  console.log(darkTheme)
 
+  return (
     <div className='relative w-full h-24 bg-[#121213] border-b-2 border-b-[#c8d4e8]' >
       <div className={'relative h-full font-TMS flex centerStage place-items-center text-white text-4xl font-extrabold tracking-wide subpixel-antialiased'}>
         Wordle
       </div>
 
       <label className="absolute min-w-fit  inline-flex justify-start items-center cursor-pointer top-1/2 left-[90%] -translate-y-1/2">
-        <input type="checkbox" value="" className="sr-only peer"/>
+        <input type="checkbox" value="" className="sr-only peer" onClick= {handleMode}/>
         <div className="z-[1] relative w-10 h-5 bg-gray-200 rounded-full 
               peer-checked:after:translate-x-full  after:content-[''] after:absolute after:top-[2px] after:left-[4px] 
         after:bg-white after:border-gray-300 after:border after:rounded-full after:h-[16px] after:w-[16px] 

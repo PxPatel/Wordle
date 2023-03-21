33eee83dd2c7fe46faf8f9179dd7713ad29da395
Priyansh <87282275+PriIrp@users.noreply.github.com>
2023-03-21 23:39:15 +0000

    Dark Mode Button Updated Feature Prototype Working

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import useLocalStorage from "./useLocalStorage";
 
 const useDarkMode = () => {
-    const [enabled, setEnabled] = useLocalStorage('dark-theme');
+    const [enabled, setEnabled] = useLocalStorage('dark-theme, true');
     const isEnabled = typeof enabledState === 'undefined' && enabled;
   
     useEffect(() => {

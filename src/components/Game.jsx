63453fc16906e46325e969d0e4f0180b6984683e
Priyansh <87282275+PriIrp@users.noreply.github.com>
2023-03-21 23:39:15 +0000

    Dark Mode Button Updated Feature Prototype Working

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -28,7 +28,7 @@ const Game = () => {
 
   // bg-[#145266]
   return (
-    <div className='flex flex-1 flex-col relative place-content-center bg-[#121213] min-h-full min-w-screen outline-none' 
+    <div className='flex flex-1 flex-col relative place-content-center bg-[#121213] dark:bg-white min-h-full min-w-screen outline-none' 
       onKeyDown={ pauses.inPlay && !pauses.loading ? handleKeyChanges : undefined }
       tabIndex={-1}
       ref= { gameRef }

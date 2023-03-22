b36e04ba7ba66c13cbcca73f5df2ee825ced9ce3
Your Name <you@example.com>
2023-03-21 22:43:11 -0400

    Dark Mode Button Icons Added Functionality Complete Adding Dark Styles

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 const useLocalStorage = (key, initialValue) => {
     const [storedValue, setStoredValue] = useState(() => {
       try {
-        const item = window.localStorage.getItem(key);
+        const item = window.sessionStorage.getItem(key);
         return item ? JSON.parse(item) : initialValue;
       } catch (error) {
         console.log(error);
@@ -17,7 +17,7 @@ const useLocalStorage = (key, initialValue) => {
   
         setStoredValue(valueToStore);
   
-        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+        window.sessionStorage.setItem(key, JSON.stringify(valueToStore));
       } catch (error) {
         console.log(error);
       }

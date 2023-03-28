fdabcdd625e39619f9466a619fcf639252e2c5e2
Priyansh <87282275+PriIrp@users.noreply.github.com>
2023-03-28 00:59:50 +0000

    Memo on Rows in Progress

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { memo } from 'react'
+import { colorScheme } from '../util/constants'
 
-const Modal = ({content, onClose}) => {
+const Modal = memo(({content, onClose}) => {
+
+  const { Modal } = colorScheme
+  console.log('Good')
 
   return (
-    <div className='centerStage absolute w-24 h-12 bg-black bg-opacity-80'>
-        <button onClick={onClose}>Hello</button>
+    <div className={`centerStage absolute w-[36rem] h-[30rem] top-[45%] left-1/2 -translate-x-1/2 -translate-y-1/2 
+     ${Modal.bgLight} ${Modal.bgDark}`}>
 
-        <div className= 'text-white'>
+        <div className= {`${Modal.textLight} ${Modal.textDark}`}>
             {content}
         </div>
 
+        <button onClick={onClose}>Close</button>
         
     </div>
   )
-}
+})
 
 export default Modal

19368293bde824662d899b2e52265f9be633b6f2
Your Name <you@example.com>
2023-04-01 17:00:17 -0400

    Modal Colors Picked

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,12 +6,12 @@ const Modal = ({content, onClose}) => {
   const { Modal } = colorScheme
 
   return (
-    <div className={`centerStage absolute w-[36rem] h-[30rem] top-[45%] left-1/2 -translate-x-1/2 -translate-y-1/2 
+    <div className={`modalRes centerStage absolute w-[30rem] h-[25rem] top-[50%] left-1/2 -translate-x-1/2 -translate-y-1/2 
      ${Modal.bgLight} ${Modal.bgDark}`}>
-        <div className= {`${Modal.textLight} ${Modal.textDark}`}>
+        <div className= {`${Modal.textLight} ${Modal.textDark} text-4xl font-TMS tracking-wider`}>
             {content}
         </div>
-        <button onClick={onClose}>Close</button>
+        <button className={`absolute bottom-[60px] px-4 py-1 border-box rounded border-2 border-indigo-700 `} onClick={onClose}>Close</button>
     </div>
   )
 }
